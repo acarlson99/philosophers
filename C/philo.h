d3e5cbec45b9ceeb75a465ed7ba3a9decf04b79f
Alexander <bombblob@gmail.com>
2019-09-13 17:49:26 -0700

    Fixed bad style.  Good clang-format

--- C/philo.h
+++ C/philo.h
@@ -9,42 +9,42 @@
 #define TIMEOUT 15
 
 enum e_state {
-  none,
-  eat,
-  rest,
-  think,
-  hold,
+	none,
+	eat,
+	rest,
+	think,
+	hold,
 };
 
 enum e_run {
-  run_wait,
-  run_go,
-  run_done,
+	run_wait,
+	run_go,
+	run_done,
 };
 
 int running;
 
 typedef struct s_stick {
-  pthread_mutex_t *m;
-  int holder;
+	pthread_mutex_t *m;
+	int holder;
 } t_stick;
 
 typedef struct s_philo {
-  int id;
-  int dead;
-  t_stick *left;
-  t_stick *right;
-  t_stick *left_hand;
-  t_stick *right_hand;
-  enum e_state state;
-  unsigned life;
+	int id;
+	int dead;
+	t_stick *left;
+	t_stick *right;
+	t_stick *left_hand;
+	t_stick *right_hand;
+	enum e_state state;
+	unsigned life;
 
-  struct s_philo *left_neighbor;
-  struct s_philo *right_neighbor;
+	struct s_philo *left_neighbor;
+	struct s_philo *right_neighbor;
 
-  // GUI stuff
-  float x;
-  float y;
+	// GUI stuff
+	float x;
+	float y;
 } t_philo;
 
 int display_visu(int num, t_philo *philos, t_stick *sticks);
--- C/visu.c
+++ C/visu.c
@@ -39,192 +39,196 @@
 #define SHAKE_MIN -SHAKE_MAX
 
 void draw_state(SDL_Renderer *renderer, SDL_Texture *circle_texture,
-                SDL_Texture *philo_texture, SDL_Texture *stick_texture, int num,
-                t_philo *philos, t_stick *sticks) {
-  static int shake = 0;
-  static int shake_direction = 1;
+				SDL_Texture *philo_texture, SDL_Texture *stick_texture, int num,
+				t_philo *philos, t_stick *sticks) {
+	static int shake = 0;
+	static int shake_direction = 1;
 
-  // table
-  SDL_SetTextureColorMod(circle_texture, TABLE_COLOR);
-  SDL_RenderCopy(
-      renderer, circle_texture, NULL,
-      &(SDL_Rect){TABLE_POS_LEFT, TABLE_POS_TOP, TABLE_WIDTH, TABLE_HEIGHT});
+	// table
+	SDL_SetTextureColorMod(circle_texture, TABLE_COLOR);
+	SDL_RenderCopy(
+		renderer, circle_texture, NULL,
+		&(SDL_Rect){TABLE_POS_LEFT, TABLE_POS_TOP, TABLE_WIDTH, TABLE_HEIGHT});
 
-  if (running == run_done) {
-    if (shake >= SHAKE_MAX || shake <= SHAKE_MIN)
-      shake_direction *= -1;
-    shake += shake_direction;
-  }
-  for (int ii = 0; ii < num; ++ii) {
-    // philo
-    float x = philos[ii].x * PHILO_CENTER_OFF + WINDOWWIDTH / 2;
-    float y = philos[ii].y * PHILO_CENTER_OFF + WINDOWHEIGHT / 2;
-    if (philos[ii].dead)
-      SDL_SetTextureColorMod(philo_texture, 0, 0, 0);
-    SDL_RenderCopyEx(renderer, philo_texture, NULL,
-                     &(SDL_Rect){x - (PHILO_SIZE / 2), y - (PHILO_SIZE / 2),
-                                 PHILO_SIZE, PHILO_SIZE},
-                     philos[ii].dead || philos[ii].state != none ? 0 : shake,
-                     NULL, SDL_FLIP_NONE);
-    SDL_SetTextureColorMod(philo_texture, 255, 255, 255);
+	if (running == run_done) {
+		if (shake >= SHAKE_MAX || shake <= SHAKE_MIN)
+			shake_direction *= -1;
+		shake += shake_direction;
+	}
+	for (int ii = 0; ii < num; ++ii) {
+		// philo
+		float x = philos[ii].x * PHILO_CENTER_OFF + WINDOWWIDTH / 2;
+		float y = philos[ii].y * PHILO_CENTER_OFF + WINDOWHEIGHT / 2;
+		if (philos[ii].dead)
+			SDL_SetTextureColorMod(philo_texture, 0, 0, 0);
+		SDL_RenderCopyEx(renderer, philo_texture, NULL,
+						 &(SDL_Rect){x - (PHILO_SIZE / 2), y - (PHILO_SIZE / 2),
+									 PHILO_SIZE, PHILO_SIZE},
+						 philos[ii].dead || philos[ii].state != none ? 0
+																	 : shake,
+						 NULL, SDL_FLIP_NONE);
+		SDL_SetTextureColorMod(philo_texture, 255, 255, 255);
 
-    // health
-    float health_x = philos[ii].x < 0 ? x - HBAR_WIDTH * 2 - PHILO_SIZE / 2
-                                      : x + HBAR_WIDTH + PHILO_SIZE / 2;
-    float health_y = y - (PHILO_SIZE / 2);
-    SDL_SetRenderDrawColor(renderer, HBAR_BKG_COLOR, 255);
-    SDL_RenderFillRect(
-        renderer, &(SDL_Rect){health_x, health_y, HBAR_WIDTH, HBAR_HEIGHT});
-    SDL_SetRenderDrawColor(renderer, HBAR_COLOR, 255);
-    float height =
-        (float)philos[ii].life / (float)MAX_LIFE * (float)HBAR_HEIGHT;
-    SDL_RenderFillRect(renderer,
-                       &(SDL_Rect){health_x, health_y + (HBAR_HEIGHT - height),
-                                   HBAR_WIDTH, height});
+		// health
+		float health_x = philos[ii].x < 0 ? x - HBAR_WIDTH * 2 - PHILO_SIZE / 2
+										  : x + HBAR_WIDTH + PHILO_SIZE / 2;
+		float health_y = y - (PHILO_SIZE / 2);
+		SDL_SetRenderDrawColor(renderer, HBAR_BKG_COLOR, 255);
+		SDL_RenderFillRect(
+			renderer, &(SDL_Rect){health_x, health_y, HBAR_WIDTH, HBAR_HEIGHT});
+		SDL_SetRenderDrawColor(renderer, HBAR_COLOR, 255);
+		float height =
+			(float)philos[ii].life / (float)MAX_LIFE * (float)HBAR_HEIGHT;
+		SDL_RenderFillRect(
+			renderer, &(SDL_Rect){health_x, health_y + (HBAR_HEIGHT - height),
+								  HBAR_WIDTH, height});
 
-    // plate
-    SDL_SetTextureColorMod(circle_texture, PLATE_COLOR);
-    float plate_x =
-        philos[ii].x * PLATE_CENTER_OFF + WINDOWWIDTH / 2 - (PHILO_SIZE / 2);
-    float plate_y =
-        philos[ii].y * PLATE_CENTER_OFF + WINDOWHEIGHT / 2 - (PHILO_SIZE / 2);
-    SDL_RenderCopy(renderer, circle_texture, NULL,
-                   &(SDL_Rect){plate_x, plate_y, PHILO_SIZE, PHILO_SIZE});
-    SDL_SetTextureColorMod(circle_texture, 255, 255, 255);
+		// plate
+		SDL_SetTextureColorMod(circle_texture, PLATE_COLOR);
+		float plate_x = philos[ii].x * PLATE_CENTER_OFF + WINDOWWIDTH / 2 -
+						(PHILO_SIZE / 2);
+		float plate_y = philos[ii].y * PLATE_CENTER_OFF + WINDOWHEIGHT / 2 -
+						(PHILO_SIZE / 2);
+		SDL_RenderCopy(renderer, circle_texture, NULL,
+					   &(SDL_Rect){plate_x, plate_y, PHILO_SIZE, PHILO_SIZE});
+		SDL_SetTextureColorMod(circle_texture, 255, 255, 255);
 
-    // sticks
-    if (sticks[ii].holder == -1) {
-      x = ((philos[ii].x + philos[(ii - 1 + num) % num].x) / 2) *
-              PHILO_CENTER_OFF +
-          WINDOWWIDTH / 2 - (PHILO_SIZE / 2) + (STICK_WIDTH / 2);
-      y = ((philos[ii].y + philos[(ii - 1 + num) % num].y) / 2) *
-              PHILO_CENTER_OFF +
-          WINDOWHEIGHT / 2 - (PHILO_SIZE / 2) + (STICK_WIDTH / 2);
-      float vec_x = -(WINDOWWIDTH / 2) + (x + STICK_WIDTH / 2);
-      float vec_y = -(WINDOWHEIGHT / 2) + (y + STICK_HEIGHT / 2);
-      SDL_RenderCopyEx(renderer, stick_texture, NULL,
-                       &(SDL_Rect){x, y, STICK_WIDTH, STICK_HEIGHT},
-                       STICK_ROT + (atan2(vec_y, vec_x) * 180 / M_PI - 90),
-                       NULL, SDL_FLIP_NONE);
-
-    } else if (sticks[ii].holder == philos[ii].id) {
-      SDL_RenderCopyEx(renderer, stick_texture, NULL,
-                       &(SDL_Rect){x - PHILO_SIZE + STICK_WIDTH / 2,
-                                   y - PHILO_SIZE / 2, STICK_WIDTH,
-                                   STICK_HEIGHT},
-                       STICK_ROT, NULL, SDL_FLIP_NONE);
-    }
-    if (sticks[(ii + 1) % num].holder == philos[ii].id) {
-      SDL_RenderCopyEx(renderer, stick_texture, NULL,
-                       &(SDL_Rect){x + STICK_WIDTH / 2, y - PHILO_SIZE / 2,
-                                   STICK_WIDTH, STICK_HEIGHT},
-                       STICK_ROT, NULL, SDL_FLIP_NONE);
-    }
-  }
+		// sticks
+		if (sticks[ii].holder == -1) {
+			x = ((philos[ii].x + philos[(ii - 1 + num) % num].x) / 2) *
+					PHILO_CENTER_OFF +
+				WINDOWWIDTH / 2 - (PHILO_SIZE / 2) + (STICK_WIDTH / 2);
+			y = ((philos[ii].y + philos[(ii - 1 + num) % num].y) / 2) *
+					PHILO_CENTER_OFF +
+				WINDOWHEIGHT / 2 - (PHILO_SIZE / 2) + (STICK_WIDTH / 2);
+			float vec_x = -(WINDOWWIDTH / 2) + (x + STICK_WIDTH / 2);
+			float vec_y = -(WINDOWHEIGHT / 2) + (y + STICK_HEIGHT / 2);
+			SDL_RenderCopyEx(renderer, stick_texture, NULL,
+							 &(SDL_Rect){x, y, STICK_WIDTH, STICK_HEIGHT},
+							 STICK_ROT +
+								 (atan2(vec_y, vec_x) * 180 / M_PI - 90),
+							 NULL, SDL_FLIP_NONE);
+		}
+		else if (sticks[ii].holder == philos[ii].id) {
+			SDL_RenderCopyEx(renderer, stick_texture, NULL,
+							 &(SDL_Rect){x - PHILO_SIZE + STICK_WIDTH / 2,
+										 y - PHILO_SIZE / 2, STICK_WIDTH,
+										 STICK_HEIGHT},
+							 STICK_ROT, NULL, SDL_FLIP_NONE);
+		}
+		if (sticks[(ii + 1) % num].holder == philos[ii].id) {
+			SDL_RenderCopyEx(renderer, stick_texture, NULL,
+							 &(SDL_Rect){x + STICK_WIDTH / 2,
+										 y - PHILO_SIZE / 2, STICK_WIDTH,
+										 STICK_HEIGHT},
+							 STICK_ROT, NULL, SDL_FLIP_NONE);
+		}
+	}
 }
 
 int display_visu(int num, t_philo *philos, t_stick *sticks) {
-  SDL_Window *window;
-  SDL_Renderer *renderer;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
 
-  SDL_Surface *circle;
-  SDL_Texture *circle_texture;
+	SDL_Surface *circle;
+	SDL_Texture *circle_texture;
 
-  SDL_Surface *philo;
-  SDL_Texture *philo_texture;
+	SDL_Surface *philo;
+	SDL_Texture *philo_texture;
 
-  SDL_Surface *stick;
-  SDL_Texture *stick_texture;
+	SDL_Surface *stick;
+	SDL_Texture *stick_texture;
 
-  // SDL stuff
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s",
-                 SDL_GetError());
-    return (1);
-  }
+	// SDL stuff
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+					 "Couldn't initialize SDL: %s", SDL_GetError());
+		return (1);
+	}
 
-  if (SDL_CreateWindowAndRenderer(WINDOWWIDTH, WINDOWHEIGHT, SDL_WINDOW_SHOWN,
-                                  &window, &renderer)) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                 "Couldn't create window and renderer: %s", SDL_GetError());
-    return (1);
-  }
-  circle = SDL_LoadBMP("assets/circle.bmp");
-  if (!circle) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load bmp: %s",
-                 SDL_GetError());
-    return (1);
-  }
-  circle_texture = SDL_CreateTextureFromSurface(renderer, circle);
+	if (SDL_CreateWindowAndRenderer(WINDOWWIDTH, WINDOWHEIGHT, SDL_WINDOW_SHOWN,
+									&window, &renderer)) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+					 "Couldn't create window and renderer: %s", SDL_GetError());
+		return (1);
+	}
+	circle = SDL_LoadBMP("assets/circle.bmp");
+	if (!circle) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load bmp: %s",
+					 SDL_GetError());
+		return (1);
+	}
+	circle_texture = SDL_CreateTextureFromSurface(renderer, circle);
 
-  philo = SDL_LoadBMP("assets/philo.bmp");
-  if (!philo) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load bmp: %s",
-                 SDL_GetError());
-    return (1);
-  }
-  philo_texture = SDL_CreateTextureFromSurface(renderer, philo);
+	philo = SDL_LoadBMP("assets/philo.bmp");
+	if (!philo) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load bmp: %s",
+					 SDL_GetError());
+		return (1);
+	}
+	philo_texture = SDL_CreateTextureFromSurface(renderer, philo);
 
-  stick = SDL_LoadBMP("assets/stick.bmp");
-  if (!stick) {
-    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load bmp: %s",
-                 SDL_GetError());
-    return (1);
-  }
-  stick_texture = SDL_CreateTextureFromSurface(renderer, stick);
-  SDL_SetTextureColorMod(stick_texture, STICK_COLOR);
+	stick = SDL_LoadBMP("assets/stick.bmp");
+	if (!stick) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load bmp: %s",
+					 SDL_GetError());
+		return (1);
+	}
+	stick_texture = SDL_CreateTextureFromSurface(renderer, stick);
+	SDL_SetTextureColorMod(stick_texture, STICK_COLOR);
 
-  running = run_go;
-  int start = 0;
-  int old = 0;
-  int delta = 0;
-  int msPerFrame = 1000 / 30; // ms / fps
-  int done = 0;
-  SDL_Event event;
-  while (!done) {
-    if (!start)
-      start = SDL_GetTicks();
-    else
-      delta = old - start;
-    if (delta < msPerFrame)
-      SDL_Delay(msPerFrame - delta);
+	running = run_go;
+	int start = 0;
+	int old = 0;
+	int delta = 0;
+	int msPerFrame = 1000 / 30; // ms / fps
+	int done = 0;
+	SDL_Event event;
+	while (!done) {
+		if (!start)
+			start = SDL_GetTicks();
+		else
+			delta = old - start;
+		if (delta < msPerFrame)
+			SDL_Delay(msPerFrame - delta);
 
-    SDL_PollEvent(&event);
-    if (event.type == SDL_QUIT) {
-      done = 1;
-    } else if (event.type == SDL_KEYDOWN) {
-      switch (event.key.keysym.sym) {
-      case SDLK_ESCAPE:
-      case SDLK_q:
-        done = 1;
-        break;
-      default:
-        // quit if time up and any non-special (ctrl alt...) key pressed
-        if (!(event.key.keysym.sym & (1 << 30)))
-          done = (running == run_done);
-      }
-    }
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, BKG_COLOR, 255);
-    SDL_RenderFillRect(renderer, NULL);
-    draw_state(renderer, circle_texture, philo_texture, stick_texture, num,
-               philos, sticks);
-    SDL_RenderPresent(renderer);
+		SDL_PollEvent(&event);
+		if (event.type == SDL_QUIT) {
+			done = 1;
+		}
+		else if (event.type == SDL_KEYDOWN) {
+			switch (event.key.keysym.sym) {
+			case SDLK_ESCAPE:
+			case SDLK_q:
+				done = 1;
+				break;
+			default:
+				// quit if time up and any non-special (ctrl alt...) key pressed
+				if (!(event.key.keysym.sym & (1 << 30)))
+					done = (running == run_done);
+			}
+		}
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, BKG_COLOR, 255);
+		SDL_RenderFillRect(renderer, NULL);
+		draw_state(renderer, circle_texture, philo_texture, stick_texture, num,
+				   philos, sticks);
+		SDL_RenderPresent(renderer);
 
-    start = old;
-    old = SDL_GetTicks();
-  }
+		start = old;
+		old = SDL_GetTicks();
+	}
 
-  // teardown
-  SDL_DestroyRenderer(renderer);
-  SDL_FreeSurface(circle);
-  SDL_DestroyTexture(circle_texture);
-  SDL_FreeSurface(philo);
-  SDL_DestroyTexture(philo_texture);
-  SDL_FreeSurface(stick);
-  SDL_DestroyTexture(stick_texture);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+	// teardown
+	SDL_DestroyRenderer(renderer);
+	SDL_FreeSurface(circle);
+	SDL_DestroyTexture(circle_texture);
+	SDL_FreeSurface(philo);
+	SDL_DestroyTexture(philo_texture);
+	SDL_FreeSurface(stick);
+	SDL_DestroyTexture(stick_texture);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
-  return (0);
+	return (0);
 }
